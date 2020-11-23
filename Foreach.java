766a1cbd0b7a79801e5183ba31578925ff1ff000
Vishnu Kumar R <69717552+VishnuKumar99@users.noreply.github.com>
2020-11-23 11:43:28 +0530

    Add files via upload

---
+++ Foreach.java
@@ -0,0 +1,10 @@
+package com.sampleprgm;
+
+public class Foreach {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        for(int k:arr){
+            System.out.println(""+k);
+        }
+    }
+}
